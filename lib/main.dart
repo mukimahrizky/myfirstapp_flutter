48bf90a28bbf436bc4c3dcef0292409c30f34db8
mukimahrizky <mukimahrizky@gmail.com>
2021-10-22 06:33:34 +0800

    Home and font asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.pink[300],
+      ),
+      body: Center(
+        child: Text(
+          'hi guys!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text('click'),
+        backgroundColor: Colors.pink,
+      ),
+    )
+  ));
 }
